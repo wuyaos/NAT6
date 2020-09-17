11842ed3845b280e2380c6bc5f1d62a102b55ca9
wuya <wufeifeng_hust@163.com>
2020-09-17 09:06:13 +0800

    修改换行编码

--- README.md
+++ README.md
@@ -3,16 +3,6 @@ Get IPv6 address by NAT6 in OpenWRT
 
 脚本适用于原版OpenWRT，LEDE和PandoraBox固件，保证设备为出厂设置，仅配置了正常联网
 
-若可以打开https://github.com/wuyaos/NAT6/blob/offline/ipv6.sh
-则直接运行下面一条命令进行安装配置
-
-##安装运行##
-
-    wget https://github.com/wuyaos/NAT6/blob/offline/ipv6.sh
-    chmod -x ipv6.sh
-    sh ipv6.sh
-    
-否则，手动下载包，将ipv6.sh和nat6文件复制进路由器，并在相应目录运行
-
-    chmod -x ipv6.sh
-    sh ipv6.sh
+手动下载包，将ipv6.sh和nat6文件复制进路由器，并在相应目录运行
+chmod -x ipv6.sh
+sh ipv6.sh
--- ipv6.sh
+++ ipv6.sh
@@ -1,38 +1,30 @@
 #!/bin/bash
 
-#Install the package kmod-ipt-nat6
-# opkg update && opkg install kmod-ipt-nat6
-wget https://github.com/wuyaos/NAT6/blob/offline/nat6
+# Install the package kmod-ipt-nat6
+opkg update && opkg install kmod-ipt-nat6
 
-#Change the first letter of the "IPv6 ULA Prefix" from f to d
+# Change the first letter of the “IPv6 ULA Prefix” from f to d
 uci set network.globals.ula_prefix="$(uci get network.globals.ula_prefix | sed 's/^./d/')"
 uci commit network
 
-#Set the DHCP server to "Always announce default router"
+# Set the DHCP server to “Always announce default router”
 uci set dhcp.lan.ra_default='1'
 uci commit dhcp
 
-#Add an init script for NAT6 by creating a new file /etc/init.d/nat6 and paste the code from the section Init Script into it
-cp -f nat6 /etc/init.d/nat6
-
-#Make the script executable and enable it
+# Add an init script for NAT6
+cp nat6 /etc/init.d/nat6
 chmod +x /etc/init.d/nat6
 /etc/init.d/nat6 enable
 
-#In addition, you may now disable the default firewall rule "Allow-ICMPv6-Forward" since it's not needed when masquerading is enabled
+# In addition, you may now disable the default firewall rule “Allow-ICMPv6-Forward” since it’s not needed when masquerading is enabled
 uci set firewall.@rule["$(uci show firewall | grep 'Allow-ICMPv6-Forward' | cut -d'[' -f2 | cut -d']' -f1)"].enabled='0'
 uci commit firewall
 
-#Edit the /etc/sysctl.d/local.conf file
-sed -i -e 's|net.ipv6.conf.default.forwarding=1|net.ipv6.conf.default.forwarding=2|' /etc/sysctl.conf
-sed -i -e 's|net.ipv6.conf.all.forwarding=1|net.ipv6.conf.all.forwarding=2|' /etc/sysctl.conf
-echo "net.ipv6.conf.default.accept_ra=2" >> /etc/sysctl.conf
-echo "net.ipv6.conf.all.accept_ra=2" >> /etc/sysctl.conf
+# 修改/etc/sysctl.conf
+echo "net.ipv6.conf.default.forwarding=2" >>/etc/sysctl.conf
+echo "net.ipv6.conf.all.forwarding=2" >>/etc/sysctl.conf
+echo "net.ipv6.conf.default.accept_ra=2" >>/etc/sysctl.conf
+echo "net.ipv6.conf.all.accept_ra=2" >>/etc/sysctl.conf
 
-#Edit /etc/firewall.user
+# 加入转发规则，编辑/etc/firewall.user
 echo "ip6tables -t nat -I POSTROUTING -s $(uci get network.globals.ula_prefix) -j MASQUERADE" >> /etc/firewall.user
-
-#Reboot to to apply the config
-echo "Congfig finished!"
-echo "Device is rebooting!"
-reboot
